06cb9952f75c1c07cd075fb0c00d38b97c67294b
RoswellCR <rosbelcr@gmail.com>
2022-07-01 16:39:05 -0400

    Passing data from child to parent

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React, {Component} from 'react';
 import './global.css'
 
 class Hijo extends Component{
+  
   manejadorClick=()=>{
-    this.props.onSaluda("pasando Datos de hijo a padre")
+     this.props.onSaluda("pasando Datos de hijo a padre");
+    
   }
   render(){
     return(
       <div className='box blue'>
         <h1>HIJO</h1>
         <button onClick={this.manejadorClick}>
-          
           Saluda
         </button>
-        
       </div>
     );
   }
@@ -21,19 +21,30 @@ class Hijo extends Component{
 
 
 class App extends Component {
-  manejador=(name)=>{
+  state = {mostarH1:false}
+  aviso=()=>{
+     ;
+  };
+  manejador = (name) => {
     alert(" Esto es: "+ name);
-
-  }
+      this.setState((state)=>this.mostarH1= true);
+  };
 
   render(){
-  return (
+  console.log(this.mostarH1)
+    return (
     <div className='box red'>
        <Hijo 
-       onSaluda={this.manejador}
+         onSaluda={this.manejador}
        />
+       <div >
+          Info:{this.mostarH1 
+          ? "Se han pasado los datos de hijo a padre" 
+          : "No se ha pasado data aun de hijo a padre"}
+       </div>
+       
     </div>
-  );
+    );
   }
 }
 
