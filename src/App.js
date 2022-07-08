b0ef91789a3edba6166c90ac16041b5733553558
RoswellCR <rosbelcr@gmail.com>
2022-07-08 17:05:51 -0400

    Making API request with fetch

--- src/App.js
+++ src/App.js
@@ -1,55 +1,40 @@
 import React, {Component} from 'react';
 import './global.css'
 
-//al hacer uso de Inputs Controlados se puede tener acceso a los datos que van entrando en
-//cada momento, por lo que podremos validarlos.
-class InputControlado extends Component {
-  state={
-    text:'',
-    //tieneError: false,
-    color: '#E8E8E8'
-  }
-  actualizar=(event)=>{
-    const text = event.target.value
-    //const tieneError = text !=='' && text.length< 5
-    let color = 'green'
-    if(text.trim()=== ''){
-      color = '#E8E8E8'
-    }
-    if(text.trim() !== '' && text.trim().length<5){
-      color = 'red'
-    }
-    this.setState({text, color});
-    console.log(text, color);
-  }
-  render(){
-    const styles = {
-      border: `1px solid ${this.state.color}`,  //'1px solid red' :'1px solid #E8E8E8',
-      padding: '0.3em 0.6em',
-      outline: 'none'
-    }
-    
-    return (
-      <input 
-        type='text'
-        value={this.state.text}
-        onChange={this.actualizar} //llamara al metodo actualizar
-        style={styles}
-        />
-    )
-  }
-}
 
+
+    
 class App extends Component {
-  
+  state = {
+    users: [],
+    cargando: true
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((res)=>res.json())
+    .then(users=>this.setState({users, cargando:false }))
+    .catch(error=>{
+      //manejo de errores
+    })
+  }
+
   render(){
-  
+    if(this.state.cargando){
+      return <h1>Cargando Datos de Usuarios....</h1>
+    }
+
     return (
     <div className='box red'>
-       <h1>Inputs Controlados</h1>
-       <InputControlado 
-        
-       />
+       <ul>
+       {this.state.users.map(user=>(
+         <li key={user.id}> 
+         {user.name}
+         <a href={`   http://${user.website}`} > Website </a> 
+         </li>)
+       )}
+       </ul>
+       
      </div>
     );
   }
