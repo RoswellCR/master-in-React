682dbce78874a7631753f8ec1e93857078b8a180
RoswellCR <rosbelcr@gmail.com>
2022-07-11 00:57:12 -0400

    working with componentDidMount, componentDidUpdate

--- src/App.js
+++ src/App.js
@@ -1,40 +1,72 @@
 import React, {Component} from 'react';
 import './global.css'
-import axios from 'axios';
 
 
+class UserDetails extends Component{
+    state = {
+       user :{},
+       isFetching: false 
+    }
+
+    componentDidMount(){
+        this.fetchData()
+    }
+    componentDidUpdate(prevProps, prevState){
+      console.log('componentDidUpdate', prevProps, prevState)
+      if (prevProps.userID!== this.props.userID){
+        this.fetchData();
+      }
+    }
+    
+    fetchData=()=>{
+      this.setState({
+        isFetching: true
+      })
+      const URL= 'https://jsonplaceholder.typicode.com/users/'+ this.props.userID
+     console.log(URL)
+     fetch(URL)
+      .then(res=> res.json(URL))
+      .then(user=>this.setState({user, isFetching: false}))
+    }
+
+  render(){
+      
+      return (
+        <div>
+          <h1>User Details</h1>
+          { this.state.isFetching 
+          ? (<h1>Haciendo fetching a data...</h1>)
+          : (<pre> {JSON.stringify(this.state.user, null, 4)} </pre>)
+          }
+        </div>
+    )
+    }
+  }
     
 class App extends Component {
   state = {
-    users: [],
-    cargando: true
+    id: 2
+  
   }
-
-  componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/users')
-    .then(res=>this.setState({users:res.data, cargando:false }))
-    .catch(error=>{
-      //manejo de errores
-    })
+  
+  aumentar=()=>{
+    this.setState(state => ( {
+      id: state.id + 1
+    } ))
   }
 
   render(){
-    if(this.state.cargando){
-      return <h1>Cargando Datos de Usuarios....</h1>
-    }
+    const {id}= this.state;
 
     return (
     <div className='box red'>
-       <ul>
-       {this.state.users.map(user=>(
-         <li key={user.id}> 
-         {user.name}
-         <a href={`   http://${user.website}`} > Website </a> 
-         </li>)
-       )}
-       </ul>
-       
-     </div>
+       <h1>componentDidUpdate</h1>
+       <h2>ID: {id}</h2>
+       <button onClick={this.aumentar}>
+          Aumentar
+       </button>
+      <UserDetails  userID={id}/>
+    </div>
     );
   }
 }
