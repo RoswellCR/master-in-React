70b52b9c6601cb2b626ec0557bc47eb9de821002
RoswellCR <rosbelcr@gmail.com>
2022-07-14 00:11:27 -0400

    Observer Pattern(using PubSub)

--- src/App.js
+++ src/App.js
@@ -1,54 +1,82 @@
 import React, {Component} from 'react';
 //import './global.css'
 // const URL= 'https://jsonplaceholder.typicode.com/users/
+import PubSub from 'pubsub-js';
 
-//No recomendado usar este tipo de patron
-class Header extends Component{
-  render(){
-    return (
-        <>
-          <p>Hijo a Padre</p>
-          <h1> Event Bubbling</h1>
-        </>
-    );
-  }
-}    
-
-class Hijo extends Component {
-  handleClick = (e) => {
-    //e.stopPropagation();
-    e.saludo = "Hola mensaje desde el hijo";
-    console.log("click en <Hijo/>");
-  };
-
-  render() {
-    return (
-      <div style={boxStyles} onClick={this.handleClick}>
-        <p>Hijo</p>
-      </div>
-    );
-  }
-}
 const boxStyles = {
   padding: '0.5em',
   margin: '0.5em',
   border: '1px solid gray',
   borderRadius: '0.3em',
   textAlign: 'center'
-} 
+}
+
+class Bisnieto extends Component {
+  state = {
+    message: "***",
+  };
+  handleClick = () => {
+    PubSub.publish("saludo", "Hola desde el bisnieto");
+  };
+  componentDidMount() {
+    PubSub.subscribe("otro evento", (e, data) => {
+      this.setState({
+        message: data.title,
+      });
+    });
+  }
+  render() {
+    return (
+      <div style={boxStyles}>
+        {this.state.message}
+        <button onClick={this.handleClick}>Nieto</button>
+      </div>
+    );
+  }
+}
+
+class Nieto extends Component {
+  render() {
+    return (
+      <div style={boxStyles}>
+        <Bisnieto />
+      </div>
+    );
+  }
+}
+class Hijo extends Component {
+  render() {
+    return (
+      <div style={boxStyles}>
+        <Nieto />
+      </div>
+    );
+  }
+}
+class Header extends Component {
+  render() {
+    return <h1>Observer Pattern(Cualquiera)</h1>;
+  }
+}
 class App extends Component {
-  handleClick = (e) => {
-    console.log('click en <Padre/>', e.saludo)
+  componentDidMount() {
+    PubSub.subscribe("saludo", (e, data) => {
+      alert(data);
+    });
+  }
+  handleClick = () => {
+    PubSub.publish("otro evento", {
+      title: "Hola desde <App/>",
+    });
+  };
+  componentWillUnmount() {
+    PubSub.unsubscribe("otro evento");
   }
 
   render() {
-
-
     return (
-      <div style={boxStyles}
-        onClick={this.handleClick}
-      >
-        
+      <div style={boxStyles}>
+        <button onClick={this.handleClick}>Padre</button>
         <Header />
         <Hijo />
       </div>
