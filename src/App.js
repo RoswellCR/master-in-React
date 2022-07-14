a719f6055ca2261e6bed37751b5e3125fc6a0237
RoswellCR <rosbelcr@gmail.com>
2022-07-14 18:25:01 -0400

    using useState

--- src/App.js
+++ src/App.js
@@ -1,74 +1,38 @@
-import React, {Component} from 'react';
+import React, { useState } from "react";
 //import './global.css'
 // const URL= 'https://jsonplaceholder.typicode.com/users/
 
-
 const boxStyles = {
-  padding: '0.5em',
-  margin: '0.5em',
-  border: '1px solid gray',
-  borderRadius: '0.3em',
-  textAlign: 'center'
-}
-//retorna{Provider, Consumer}
-  const {Provider, Consumer} = React.createContext();
+  padding: "0.5em",
+  margin: "0.5em",
+  border: "1px solid gray",
+  borderRadius: "0.3em",
+  textAlign: "center",
+};
 
+const Header = () => {
+  return <h1 style={boxStyles}> Hook *useState* </h1>;
+};
 
-class Nieto extends Component {
-  
-  render() {
-    return (
-      <Consumer>
-        {(context)=>(
-          <div style={boxStyles}>
-            <h1>Nieto</h1>
-            <button onClick={context.addClicks} >Disparar {context.clicks}</button>
-          </div>
-        )}
-      </Consumer>
-    );
-  }
-}
-class Hijo extends Component {
-  render() {
-    return (
-      <div style={boxStyles}>
-        <h1>Hijo</h1>
-        <Nieto />
-      </div>
-    );
-  }
-}
-class Header extends Component {
-  render() {
-    return <h1>Context API</h1>;
-  }
-}
-class App extends Component {
-  state={
-    clicks:0
-  }
-  addClicks=()=>{
-    this.setState(state=>({clicks: state.clicks +1} ))
-  }
+const App = () => {
+  const [clicks, setClicks] = useState(0);
+  const [title, setTitle] = useState("");
 
-  render() {
-    return (
-      <Provider 
-        value={{
-          clicks:this.state.clicks,
-          addClicks:this.addClicks
-        }}
-        
-        >
-        <div style={boxStyles}>
-          <Header />
-          <Hijo />
-        </div>
-      </Provider>
-      
-    );
-  }
-}
+  const addClicks = () => {
+    setClicks(clicks + 1);
+  };
 
+  const handleInput = (e) => {
+    setTitle(e.target.value);
+  };
+
+  return (
+    <>
+      <Header />
+      <input type="text" value={title} onChange={handleInput} />
+      <button onClick={addClicks}> Clicks {clicks}</button>
+      <h3>{title}</h3>
+    </>
+  );
+};
 export default App;
