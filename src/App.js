e11e83a2c7ccc02887730adb682af6450c62a8ed
RoswellCR <rosbelcr@gmail.com>
2022-07-15 01:27:30 -0400

    useEfect with fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 //import './global.css'
-// const URL= 'https://jsonplaceholder.typicode.com/users/
+
 
 const boxStyles = {
   padding: "0.5em",
@@ -15,27 +15,33 @@ const Header = () => {
 };
 
 const App = () => {
-  const [clicks, setClicks] = useState(0)
-  const [emoji, setEmoji] = useState('🌎')
-
-  const handleClick=()=>{
-  setClicks(clicks+1)
- }
- const toggleEmoji=()=>{
-   const nextEmoji = emoji === '🌎'? '😍' : '🌎'
-   setEmoji(nextEmoji)
- }
+  const [users, setUsers] = useState([])
+  const [isFetching,setIsFetching] = useState(true); 
 
   useEffect(()=>{
-    alert('usando useEfect cuando cambie el emoji')
-  },[emoji])
+    fetch('https://jsonplaceholder.typicode.com/users/')
+   .then(res=>res.json())
+   .then(users=> {
+     setUsers(users) 
+     setIsFetching(false)})
+   console.log(users)
+  },[])
 
   return (
     <>
       <Header />
-      <button onClick={handleClick}> ADD {clicks} </button>
-      <button onClick={toggleEmoji}> Alternar Emoji{emoji} </button>
-      <h1>{emoji}</h1>
+      {isFetching && <h1>Cargando Datos...</h1>} 
+      <ul>
+      {users.map(user=>(
+        <li key={user.id}>
+          {user.name}
+        </li>
+      ))
+
+      }
+      </ul>
+      
+      <h1>{}</h1>
     </>
   );
 };
