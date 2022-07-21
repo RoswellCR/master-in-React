9309703eb370b98653593b7894c57c97b4c47fb5
RoswellCR <rosbelcr@gmail.com>
2022-07-21 01:03:50 -0400

    r-router-dom(rutas dinamicas con params)

--- src/App.js
+++ src/App.js
@@ -1,75 +1,76 @@
 import React from 'react'
-import { BrowserRouter, Route, Link , NavLink } from 'react-router-dom'
-
-const Hola = () => {
-  return (<h1>Hola</h1>)
-}
-
-const Productos = () => {
-  return <h1>Productos</h1>
-}
+import { BrowserRouter, Route, NavLink, Link } from 'react-router-dom'
 
 
-const Home = (props) => {
-  console.log(props);
-  return(
-    <h1>Home</h1>
+const Hola = () => (
+  <h1>Hola</h1>
+)
+
+const Productos = (props) => (
+  <div>
+    <h1>Productos</h1>
+    <Link to='/productos/gamers'>Gamers</Link>
+    <Link to='/productos/hogar'>Hogar</Link>
+  </div>
+)
+
+const Home = (props) => (
+  <h1>Home</h1>
+)
+
+const ProductosCategoria = ({ match }) => {
+  console.log(match)
+
+  return (
+    <div>
+      <h1>Categoria: { match.params.categoria }</h1>
+    </div>
   )
 }
 
-
-const Navegation = ()=> {
- //console.log("navigation");
- return (
-   <nav>
-     <NavLink
-       to="/"
-       exact
-       activeStyle={{ color: "orangered" }}
-       activeClassName={{ color: "green" }}
-     >
-       {" "}
-       Home{" "}
-     </NavLink>
-     <NavLink
-       to="/hola"
-       activeStyle={{ color: "orangered" }}
-       activeClassName={{ color: "green" }}
-     >
-       {" "}
-       Hola{" "}
-     </NavLink>
-     <NavLink to="/productos" 
-     activeClassName={{ color: "green" }}
-     isActive={(match, location)=>{
-       return false
-       }}>
-       {" "}
-       Productos{" "}
-     </NavLink>
-   </nav>
- );
+const navStyles = {
+  display: 'flex',
+  justifyContent: 'space-around'
 }
 
+const NavActive = {
+  color: 'orangered'
+}
+
+const Navegation = () => (
+  <nav style={navStyles}>
+    <NavLink
+      to='/'
+      exact
+      activeStyle={NavActive}
+    >
+      Home
+    </NavLink>
+    <NavLink
+      to='/hola'
+      activeClassName='navActive'
+    >
+      Hola
+    </NavLink>
+    <NavLink
+      to='/productos'
+      activeStyle={NavActive}
+    >
+      Productos
+    </NavLink>
+  </nav>
+)
 
 const App = () => {
   return (
     <BrowserRouter>
       <Navegation />
-      <Route
-          path='/'
-          exact
-          render={Home}
-        />
-      <Route
-        path='/hola'
-        render={Hola}
-      />
-      <Route
-          path='/productos'
-        render={Productos}/>
-      </BrowserRouter>
+      <Route path='/' exact render={Home} />
+      <Route path='/hola' render={Hola} />
+      <Route path='/productos' exact render={Productos} />
+      <Route path='/productos/:categoria/:id?' render={ProductosCategoria} />
+    </BrowserRouter>
   )
 }
 
-export default App;
+export default App
