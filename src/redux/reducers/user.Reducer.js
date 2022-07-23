bf16b4039aca962dd35f2ea324b54a9f91d8ea0c
RoswellCR <rosbelcr@gmail.com>
2022-07-22 21:34:51 -0400

    manage the state using objects

--- src/redux/reducers/user.Reducer.js
+++ src/redux/reducers/user.Reducer.js
@@ -1,9 +1,19 @@
+import {UPDATE_NAME} from '../actions/user.Action'
 
-function user (state={name: 'Rosbel'}, action){
+const initialState= {
+    name: 'Rosbel',
+    country: 'Cuba'
+}
+
+function user (state=initialState, action){
     switch (action.type) {
+        case UPDATE_NAME:
+            return {
+                ...state,
+                name: action.payload.name,
+            }
         default:
-           
-           return state 
+          return state 
     }
 }
 
