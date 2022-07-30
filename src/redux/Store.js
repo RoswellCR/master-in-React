d342e3e5a48721cbe10d2b9f9ce16a3eefdc6a03
RoswellCR <rosbelcr@gmail.com>
2022-07-30 18:29:54 -0400

    Redux-Midlewares

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,9 +1,22 @@
-import { createStore } from "redux";
+import { createStore , applyMiddleware} from "redux";
 //import { INCREMENT, DECREMENT } from "./reducers/counter";
 import rootReducer from './reducers'
 
+const confirmDeleteTodo=(store)=>(next)=>(action)=>{
+    console.log('se va a realizar una nueva accion', action)
+    if(action.type==='DELETE_TODO'){
+         let conf = window.confirm('Seguro que quiere eliminar el todo')
+         if(conf){
+             next(action)
+         }
+    }
+    else{
+        next(action)
+    }
+}
+
 
 //ALMACENA EL ESTADO
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(confirmDeleteTodo))
 
 export default store;
